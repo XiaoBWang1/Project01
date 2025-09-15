72af22a7b002edd0d9d43867d491fdb5b24008d2
GoodGuyGregory <greg.witt625@gmail.com>
2025-09-14 22:16:49 -0400

    adds playe round button and basic play_btn for play logic in the game

--- main.py
+++ main.py
@@ -6,15 +6,34 @@ import gradio as gr
 options = ("rock", "paper", "scissor")
 game_status = True
 
+player_wins = 0
+computer_wins = 0
+
 with gr.Blocks() as demo:
     gr.Markdown(
         """
             # Rock Paper Scissors
+            
         """
     )
+    
+    gr.Markdown(
+        f"""
+            | Human | Computer | 
+            | -------| ------|
+            | 👤 | 🤖 | 
+            | {player_wins} | {computer_wins} |
+        """
+    )
+    
 
     with gr.Row():
         player_choice = gr.Dropdown(['rock', 'paper', 'scissors'], label="Choose your move", multiselect=False, interactive=True,)
+    
+    with gr.Row():
+        play_btn = gr.Button("Play Round")
+        play_btn.click()
+        
         
 if __name__ == "__main__":
     
