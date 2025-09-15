1d3a80c1f31dd2185308451b4edd4436ffbf87ab
xbw24 <xbw24@yahoo.com>
2025-09-14 21:22:15 -0400

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+# import random libray, rock/paper/scissor game
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Rock, paper, and scissor game against computer. Set a true variable for game
+options = ("rock", "paper", "scissor")
+game_status = True
 
+# While loop with user input and random computer input
+while game_status:
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    player = None
+    computer = random.choice(options)
 
+    while player not in options:
+        player = input("Select rock, paper, or scissor: ")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Displays format string of player and computer input
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# If/else statement for win, tie, or lose scenarios with print statements
+    if player == computer:
+        print("Both have the same pick. Try again.")
+    elif player == "Rock" and computer == "Scissor":
+        print("You win!")
+    elif player == "Paper" and computer == "Rock":
+        print("You win!")
+    elif player == "Scissor" and computer == "paper":
+        print("You win!")
+    else:
+        print("You've lost!")
+
+# If statement to break out of the loop using Boolean
+    if not input("Try again(y/n)? ").lower() == "y":
+        game_status = False
+
+    print("Thanks for playing!")
